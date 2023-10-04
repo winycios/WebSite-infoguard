1c339522d7357dc1a6d25923530434a04b761155
Winycios <winycios.nascimento@sptech.school>
2023-10-04 17:12:54 -0300

    Atualização do banco de dadod

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -1,49 +1,37 @@
 -- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
-/*
-comandos para mysql - banco local - ambiente de desenvolvimento
-*/
-
 CREATE DATABASE SuperVisiON;
 
 USE SuperVisiON;
 
 CREATE TABLE tbOrganizacao(
 
-idOrganizacao INT PRIMARY KEY AUTO_INCREMENT,
-nome VARCHAR(100),
-cnpj CHAR(14)
+cnpj CHAR(14) PRIMARY KEY,
+nome VARCHAR(100)
 );
-
-insert into tbOrganizacao values (null, "pinto", "123");
-SELECT cpf, nome, fk_idOrganizacao as organizacaoId FROM tbUsuario WHERE cpf = '${cpf}' AND senha = '${senha}';
-
+	
 CREATE TABLE tbUsuario(
 
 cpf CHAR(11) PRIMARY KEY ,
-fk_idOrganizacao INT,
+fk_organizacao CHAR(14),
 nome VARCHAR(100),
 senha VARCHAR(20),
 telefone CHAR(11),
 cargo VARCHAR(50),
 statusServico VARCHAR(45),
-FOREIGN KEY (fk_idOrganizacao) REFERENCES tbOrganizacao(idOrganizacao)
+FOREIGN KEY (fk_organizacao) REFERENCES tbOrganizacao(cnpj)
 );
-
-select * from tbUsuario;
+	
 CREATE TABLE tbEvento(
 
 idEvento INT PRIMARY KEY AUTO_INCREMENT,
-fk_idOrganizacao INT,
+fk_organizacao CHAR(14),
 nome VARCHAR(100),
-data DATE,
 time1 VARCHAR(50),
 time2 VARCHAR(50),
-FOREIGN KEY (fk_idOrganizacao) REFERENCES tbOrganizacao(idOrganizacao)
+status VARCHAR(20),
+FOREIGN KEY (fk_organizacao) REFERENCES tbOrganizacao(cnpj)
 );
-
+    
 CREATE TABLE tbComputadores(
 
 idComputador INT PRIMARY KEY AUTO_INCREMENT,
@@ -76,11 +64,6 @@ disco INT,
 ram INT,
 FOREIGN KEY (fk_idComputador) REFERENCES tbComputadores(idComputador)
 );
-/*
-comandos para criar usuário em banco de dados azure, sqlserver,
-com permissão de insert + update + delete + select
-*/
-
 create user 'superOn'@'localhost' identified by '123';
 grant all privileges on SuperVisiON.* to 'superOn'@'localhost';
 flush privileges;
